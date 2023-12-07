49ab9cb28254ffd620846d0fe674c33f3f608596
Robert Patterson <robert@robertgpatterson.com>
2023-12-07 13:52:24 -0600

    changes for 0.69

--- html/search/enums_4.js
+++ html/search/enums_4.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['enclosuremodes_0',['ENCLOSUREMODES',['../class_f_c_enclosure.html#a109c0798d731a1b7db0964b2883c2ebb',1,'FCEnclosure']]],
   ['enclosureshape_1',['ENCLOSURESHAPE',['../class_f_c_enclosure.html#abd2d0bb2e9f9f5aa9f99f43eefcae399',1,'FCEnclosure']]],
-  ['entry_5fdurations_2',['ENTRY_DURATIONS',['../class_f_c_note_entry.html#ab926ef2d75889d1cfcb3cca6a426d04d',1,'FCNoteEntry']]],
-  ['execmodal_5freturns_3',['EXECMODAL_RETURNS',['../class_f_c_resource_window.html#ade11f0bf17049792709196671044afc2',1,'FCResourceWindow']]],
-  ['expr_5fplayback_5ftypes_4',['EXPR_PLAYBACK_TYPES',['../class_f_c_text_expression_def.html#ab76cd672bcf68373b8aaad8c3216531e',1,'FCTextExpressionDef']]],
-  ['expression_5fplayback_5fstartpoints_5',['EXPRESSION_PLAYBACK_STARTPOINTS',['../class_f_c_expression.html#ad9ff8705b184a052d594836ac4e3e2c2',1,'FCExpression']]],
-  ['extract_5fspacing_6',['EXTRACT_SPACING',['../class_f_c_part_extract_prefs.html#a3ebfdca199476fa3f336f89d6ca12e09',1,'FCPartExtractPrefs']]]
+  ['enigmastyles_2',['ENIGMASTYLES',['../class_f_c_font_info.html#ac52dd376a24069038ac379c8b8fe8cdd',1,'FCFontInfo']]],
+  ['entry_5fdurations_3',['ENTRY_DURATIONS',['../class_f_c_note_entry.html#ab926ef2d75889d1cfcb3cca6a426d04d',1,'FCNoteEntry']]],
+  ['execmodal_5freturns_4',['EXECMODAL_RETURNS',['../class_f_c_resource_window.html#ade11f0bf17049792709196671044afc2',1,'FCResourceWindow']]],
+  ['expr_5fplayback_5ftypes_5',['EXPR_PLAYBACK_TYPES',['../class_f_c_text_expression_def.html#ab76cd672bcf68373b8aaad8c3216531e',1,'FCTextExpressionDef']]],
+  ['expression_5fplayback_5fstartpoints_6',['EXPRESSION_PLAYBACK_STARTPOINTS',['../class_f_c_expression.html#ad9ff8705b184a052d594836ac4e3e2c2',1,'FCExpression']]],
+  ['extract_5fspacing_7',['EXTRACT_SPACING',['../class_f_c_part_extract_prefs.html#a3ebfdca199476fa3f336f89d6ca12e09',1,'FCPartExtractPrefs']]]
 ];
